b60240feab64a3af4dffe7192c8b35e2b552bb11
TejaswiYalala <yalalatejaswi@gmail.com>
2024-11-21 12:54:16 -0600

    Add files via upload

---
+++ Insertionsorting.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+//Insertion Sorting
+void sort(int numbers[], int n) 
+{
+    for (int i=1; i<n; i++) 
+    {
+        int x = numbers[i];
+        int j = i-1;
+        while (j >= 0 && numbers[j] > x) 
+        {
+            numbers[j + 1] = numbers[j];
+            j = j-1;
+        }
+        numbers[j+1] = x;
+    }
+}
+//  find the median of the numbers
+float sortAndFindMedian(int numbers[], int n)
+{
+    sort(numbers, n);
+    // n is even, median is the average of middle values
+    if (n % 2 == 0) {
+        return (numbers[n/2 - 1] + numbers[n/2])/2;
+    }
+    // If n is odd, median= middle value
+    else {
+        return numbers[n/2];
+    }
+}
+
+int main() 
+{
+    int n;
+    printf("Enter the size of array: ");
+    scanf("%d", &n);
+    int numbers[n];
+    printf("Enter the elements: ");
+    for (int i = 0; i<n ; i++) 
+    {
+        scanf("%d", &numbers[i]);
+    }
+    float median = sortAndFindMedian(numbers, n);
+    printf("The median of the given numbers is: %.2f\n", median);
+    return 0;
+}
